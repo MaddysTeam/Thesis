2dbc595ef244e76f67d4c2515faf995f32ec1bf5
JimmyPoor <kissnana3@163.com>
2020-07-10 17:34:46 +0800

    20200710

--- Res/Business/Utils/ResDeliveryHelper.cs
+++ Res/Business/Utils/ResDeliveryHelper.cs
@@ -1,64 +1,64 @@
 ﻿namespace Res.Business
 {
 
-   /// <summary>
-   /// resouce delivery helper
-   /// </summary>
+	/// <summary>
+	/// resouce delivery helper
+	/// </summary>
 	public static class ResDeliveryHelper
-   {
+	{
 
-      public static int GetDeliveryCount(long provinceId, long areaId, APDBDef db = null)
-      {
-         var session = ResSettings.SettingsInSession;
-         if (session == null)
-            return 0;
+		public static int GetDeliveryCount(long provinceId, long areaId, APDBDef db = null)
+		{
+			var session = ResSettings.SettingsInSession;
+			if (session == null)
+				return 0;
 
-         var r = APDBDef.CroResource;
-         Symber.Web.Data.APSqlWherePhrase where = null;
-         db = db ?? new APDBDef();
+			var r = APDBDef.CroResource;
+			Symber.Web.Data.APSqlWherePhrase where = null;
+			db = db ?? new APDBDef();
 
-         if (session.IsAdmin)
-            where=(r.DeliveryStatus == CroResourceHelper.IsDelivery);
+			if (session.IsAdmin)
+				where = (r.DeliveryStatus == CroResourceHelper.IsDelivery);
 
-         if (provinceId > 0)
-            where = where & r.ProvinceId == provinceId;
+			else if (provinceId > 0)
+				where = r.ProvinceId == provinceId;
 
-         if (areaId > 0)
-            where = where & r.AreaId == areaId;
+			else if (areaId > 0)
+				where = where & r.AreaId == areaId;
 
-         return db.CroResourceDal.ConditionQueryCount(where);
-      }
+			return db.CroResourceDal.ConditionQueryCount(where);
+		}
 
 
-      public static bool IsExccedMaxCount(long provinceId, long areaId, int allowdMaxCount, APDBDef db)
-      {
-         var count = GetDeliveryCount(provinceId, areaId, db);
-         return count > allowdMaxCount;
-      }
+		public static bool IsExccedMaxCount(long provinceId, long areaId, int allowdMaxCount, APDBDef db)
+		{
+			var count = GetDeliveryCount(provinceId, areaId, db);
+			return count > allowdMaxCount;
+		}
 
-      /// <summary>
-      /// get allowd max resource delivery count for province and area admin
-      /// </summary>
-      /// <returns></returns>
-      public static int GetMaxAllowCount()
-      {
-         var session = ResSettings.SettingsInSession;
-         if (session == null)
-            return 0;
+		/// <summary>
+		/// get allowd max resource delivery count for province and area admin
+		/// </summary>
+		/// <returns></returns>
+		public static int GetMaxAllowCount()
+		{
+			var session = ResSettings.SettingsInSession;
+			if (session == null)
+				return 0;
 
-         int maxCount = 0;
-         var user = session.User;
+			int maxCount = 0;
+			var user = session.User;
 
-         if (session.IsAdmin)
-            maxCount = int.MaxValue;
-         if ((session.IsProvinceAdmin || session.IsCityAdmin) && user.ProvinceId == ResCompanyHelper.Shanghai) //是上海市区级管理员，每个区允许80篇
-            maxCount = ResSettings.SettingsInSession.IsCityAdmin ? ThisApp.DeliveryCountForShanghaiArea : 16 * ThisApp.DeliveryCountForShanghaiArea;
-         else if (session.IsProvinceAdmin && user.ProvinceId != ResCompanyHelper.Shanghai) //是省管理员，每个省允许100篇
-            maxCount = ThisApp.DeliveryCountForPerOtherProvince;
+			if (session.IsAdmin)
+				maxCount = int.MaxValue;
+			if ((session.IsProvinceAdmin || session.IsCityAdmin) && user.ProvinceId == ResCompanyHelper.Shanghai) //是上海市区级管理员，每个区允许80篇
+				maxCount = ResSettings.SettingsInSession.IsCityAdmin ? ThisApp.DeliveryCountForShanghaiArea : 16 * ThisApp.DeliveryCountForShanghaiArea;
+			else if (session.IsProvinceAdmin && user.ProvinceId != ResCompanyHelper.Shanghai) //是省管理员，每个省允许100篇
+				maxCount = ThisApp.DeliveryCountForPerOtherProvince;
 
-         return maxCount;
-      }
+			return maxCount;
+		}
 
-   }
+	}
 
 }
