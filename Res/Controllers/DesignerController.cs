07c6c2abf45b330b5b5d458a0785111cd481da22
JimmyPoor <kissnana3@163.com>
2020-12-01 17:36:50 +0800

    20201201

--- Res/Controllers/DesignerController.cs
+++ Res/Controllers/DesignerController.cs
@@ -24,22 +24,24 @@ namespace Res.Controllers
 
 			// return new BinaryContentResult($"aaa.pdf", "application/pdf", pdfFile);
 
-			//CreateAndBindMedal();
+			CreateAndBindMedal();
 
-			CreateCompanyMedal();
+			//CreateCompanyMedal();
 
 			return View();
 		}
 
 		private void CreateCompanyMedal()
 		{
-			String[] comapnyNames = { "上海市闵行区教育学院" };
+			String[] comapnyNames = {
+				"江苏省扬州市电化教育馆"
+			};
 
 			//var winlevelResources = db.CroResourceDal.ConditionQuery(c.Author == "张成娟", null, null, null);
 			var i = 0;
 			foreach (var item in comapnyNames)
 			{
-				if (i > 10) break;
+				//if (i > 10) break;
 
 				var md5 = string.Empty;
 				var filePath = Server.MapPath("~/Attachments/medal.html");
@@ -55,7 +57,7 @@ namespace Res.Controllers
 					var docFile = new UploadFile
 					{
 						Stream = fs,
-						FileName = $"2019/files/{DateTime.Today.ToString("yyyyMMdd")}/{md5}{FileHelper.GifExtName}"
+						FileName = $"2020/files/{DateTime.Today.ToString("yyyyMMdd")}/{md5}{FileHelper.GifExtName}"
 					};
 					var docResult = FileUploader.SliceUpload(docFile);
 					if (!docResult.IsSuccess)
@@ -74,52 +76,88 @@ namespace Res.Controllers
 			var c = APDBDef.CroResource;
 			var f = APDBDef.Files;
 
-			var winlevelResources = db.CroResourceDal.ConditionQuery(c.Author == "张成娟", null, null, null);
+			var winlevelResources = db.CroResourceDal.ConditionQuery(c.WinLevelPKID == 205, null, null, null);
 			var i = 0;
-			foreach (var item in winlevelResources)
+
+			db.BeginTrans();
+
+
+			try
 			{
-				if (i > 10) break;
-
-				var md5 = string.Empty;
-				var filePath = Server.MapPath("~/Attachments/medal.html");
-				var htmlStr = System.IO.File.ReadAllText(filePath).Replace("{{Auther}}", item.Author)
-				   .Replace("{{ResourceTitle}}", item.Title)
-				   .Replace("{{WinLevel}}", "二 等 奖")
-				   .Replace("{{MedalCode}}", $"");
-				var fs = MentalConverter.ConverHtmlToImage(htmlStr, item.Title, out md5);
-				using (fs)
+				foreach (var item in winlevelResources)
 				{
-					if (fs.Length <= 0)
-					{
-						Console.Write($"{item.Title} creat fail");
-						break;
-					}
+					//if (i > 0) break;
 
-					var fileIsExist = db.FilesDal.ConditionQueryCount(f.Md5 == md5) > 0;
-					if (fileIsExist)
-						break;
+					var md5 = string.Empty;
+					var filePath = Server.MapPath("~/Attachments/medal.html");
+					var htmlStr = System.IO.File.ReadAllText(filePath).Replace("{{Auther}}", item.Author)
+					   .Replace("{{ResourceTitle}}", item.Title)
+					   .Replace("{{WinLevel}}", "一 等 奖")
+					   .Replace("{{MedalCode}}", $"");
+					var fs = MentalConverter.ConverHtmlToImage(htmlStr, item.Title, out md5);
+					using (fs)
+					{
+						if (fs.Length <= 0)
+						{
+							Console.Write($"{item.Title} creat fail");
+							break;
+						}
 
-					var docFile = new UploadFile
-					{
-						Stream = fs,
-						FileName = $"2019/files/{DateTime.Today.ToString("yyyyMMdd")}/{md5}{FileHelper.GifExtName}"
-					};
-					var docResult = FileUploader.SliceUpload(docFile);
-					if (docResult.IsSuccess)
-					{
+						var fileIsExist = db.FilesDal.ConditionQueryCount(f.Md5 == md5) > 0;
+						if (fileIsExist)
+							break;
+
+						//var docFile = new UploadFile
+						//{
+						//	Stream = fs,
+						//	FileName = $"2019/files/{DateTime.Today.ToString("yyyyMMdd")}/{md5}{FileHelper.GifExtName}"
+						//};
+						db.BeginTrans();
+
+						string path = $@"E:\项目文件(GitLab)\长三角新版(论文活动)\Site\Attachments\level1\{md5}{FileHelper.GifExtName}";
+						using (fs)
+						{
+							byte[] buffer = new byte[fs.Length];
+							fs.Seek(0, SeekOrigin.Begin);
+							fs.Read(buffer, 0, (int)buffer.Length);
+
+							using (FileStream fs2 = new FileStream(path, FileMode.OpenOrCreate))
+							{
+								fs2.Write(buffer, 0, buffer.Length);
+								fs2.Flush();
+							}
+						}
+
+						var file = new Files { FileName = $"{item.Title}的奖章", FilePath = $"/Attachments/level1/{md5}{FileHelper.GifExtName}", ExtName = FileHelper.GifExtName, Md5 = md5 };
+						db.FilesDal.Insert(file);
+						db.CroResourceMedalDal.Insert(new CroResourceMedal { FileId = file.FileId, CrosourceId = item.CrosourceId, ActiveId = 1001, CreateDate = DateTime.Now });
+
+
+
+						//var docResult = FileUploader.SliceUpload(path);
+						//if (docResult.IsSuccess)
+						//{
 						//var file = new Files { FileName = $"{item.Title}的奖章", FilePath = docResult.FileUrl, ExtName = FileHelper.GifExtName, Md5 = md5 };
 						//db.FilesDal.Insert(file);
 						//db.CroResourceMedalDal.Insert(new CroResourceMedal { FileId = file.FileId, CrosourceId = item.CrosourceId, ActiveId = 2, CreateDate = DateTime.Now });
+						//}
+						//else
+						//{
+						//	Console.Write($"{item.Title} mendal upload fail");
+						//	break;
+						//}
 					}
-					else
-					{
-						Console.Write($"{item.Title} mendal upload fail");
-						break;
-					}
+
+					i++;
 				}
 
-				i++;
+				db.Commit();
 			}
+			catch
+			{
+				db.Rollback();
+			}
+			
 		}
 
 		private Dictionary<long, string> CodeMappings = new Dictionary<long, string>()
